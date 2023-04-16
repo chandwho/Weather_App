d6664ac029466c499ba2d8f42519b8f60f2881ef
Chandan <ch7.pandey@gmail.com>
2023-04-17 02:26:43 +0530

    -- Made website responsive to small screens -- Fixed footer to bottom

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 /* eslint-disable no-console */
 /* eslint-disable camelcase */
 
-const input = document.querySelector("#search");
-const form = document.querySelector("form");
+const input = document.querySelector('#search');
+const form = document.querySelector('form');
 
-const description = document.querySelector("#description");
-const temperature = document.querySelector("#temp");
-const feelTemp = document.querySelector("#feel-temp");
-const humidity = document.querySelector("#humidity");
-const windSpeed = document.querySelector("#wind-speed");
-const locationInfo = document.querySelector("#location-info");
-const weatherIcon = document.querySelector("#weather-icon");
+const description = document.querySelector('#description');
+const temperature = document.querySelector('#temp');
+const feelTemp = document.querySelector('#feel-temp');
+const humidity = document.querySelector('#humidity');
+const windSpeed = document.querySelector('#wind-speed');
+const locationInfo = document.querySelector('#location-info');
+const weatherIcon = document.querySelector('#weather-icon');
 
-//Shows weather of default city
+// Shows weather of default city
 async function defaultWeatherData() {
   const location = input.value;
   console.log(location);
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=almora&appid=5d5a640d5303a85ba1b0ed9bb0b036ca&units=metric`,
-    { mode: "cors" }
+    'https://api.openweathermap.org/data/2.5/weather?q=almora&appid=5d5a640d5303a85ba1b0ed9bb0b036ca&units=metric',
+    { mode: 'cors' },
   );
 
   const weatherData = await response.json();
   console.log(weatherData);
-  temperature.innerText = weatherData.main.temp + " °C";
-  feelTemp.innerText = weatherData.main.feels_like + " °C";
-  humidity.innerText = weatherData.main.humidity + "%";
-  windSpeed.innerText = weatherData.wind.speed + " m/sec";
+  temperature.innerText = `${weatherData.main.temp} °C`;
+  feelTemp.innerText = `${weatherData.main.feels_like} °C`;
+  humidity.innerText = `${weatherData.main.humidity}%`;
+  windSpeed.innerText = `${weatherData.wind.speed} m/sec`;
   description.innerText = upper(weatherData.weather[0].description);
   weatherIcon.src = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
-  locationInfo.innerText = weatherData.name + ", " + weatherData.sys.country;
+  locationInfo.innerText = `${weatherData.name}, ${weatherData.sys.country}`;
 }
 
-form.addEventListener("submit", showWeatherData);
+form.addEventListener('submit', showWeatherData);
 
 async function getWeatherData() {
   const location = input.value;
   console.log(location);
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=5d5a640d5303a85ba1b0ed9bb0b036ca&units=metric`,
-    { mode: "cors" }
+    { mode: 'cors' },
   );
   return response;
 }
@@ -51,16 +51,15 @@ function showWeatherData(e) {
     .then((res) => res.json())
     .then((weatherData) => {
       console.log(weatherData);
-      temperature.innerText = weatherData.main.temp + " °C";
-      feelTemp.innerText = weatherData.main.feels_like + " °C";
-      humidity.innerText = weatherData.main.humidity + "%";
-      windSpeed.innerText = weatherData.wind.speed + " m/sec";
+      temperature.innerText = `${weatherData.main.temp} °C`;
+      feelTemp.innerText = `${weatherData.main.feels_like} °C`;
+      humidity.innerText = `${weatherData.main.humidity}%`;
+      windSpeed.innerText = `${weatherData.wind.speed} m/sec`;
       description.innerText = upper(weatherData.weather[0].description);
       weatherIcon.src = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
-      locationInfo.innerText =
-        weatherData.name + ", " + weatherData.sys.country;
+      locationInfo.innerText = `${weatherData.name}, ${weatherData.sys.country}`;
     });
-  input.value = "";
+  input.value = '';
 }
 
 function upper(str) {
